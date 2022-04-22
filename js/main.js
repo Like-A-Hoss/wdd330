326f458212baf80c70bc8e9f2da9d65f02b17eb1
Like-A-Hoss <hapleswithdice@gmail.com>
2022-04-22 12:15:57 -0500

    adding additional files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+///JavaScript to set links on pages
+const links = [
+    {
+        label: "Week1 Notes",
+        url: "week1/index.html"
+    }
+];
+for (text in links){
+    var message = "<li><a href='" + text.url + "'>"+ text.label +"</a></li>" ;
+    document.get
+}
