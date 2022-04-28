257de497f1004ec8492e71359e41c814f04c692b
Like-A-Hoss <hapleswithdice@gmail.com>
2022-04-27 20:01:30 -0500

    Snippets 2

---
+++ snippetts/header.php
@@ -0,0 +1,3 @@
+
+        <h1>WDD 340 Assignments of Nathan Hoskins</h1>
+        <p>A portfollio of work made for class.</p>
---
+++ snippetts/nav.php
@@ -0,0 +1,4 @@
+<p>Assignments</p>
+        <ol style="text-align: center; list-style-position: inside;" id="navList">
+            
+        </ol>
