48759bcdbcb290636015d0c32f5d36a22d0574b3
Like-A-Hoss <hapleswithdice@gmail.com>
2022-05-07 11:35:53 -0500

    Added client side snippets to create header and footer

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const links = [
     {
         label: "Week2 Notes",
         url: "../week2/index.html"
+    },
+    {
+        label: "Week 3 Notes",
+        url: "../week3/index.html"
     }
 ];
 let message = " ";
@@ -15,3 +19,9 @@ for (text in links){
     message += link;
 }
 document.getElementById('navList').innerHTML = message;
+
+const header_Generator = " <h1>WDD 340 Assignments of Nathan Hoskins</h1> <p>A portfollio of work made for class.</p> "
+document.getElementById('header').innerHTML = header_Generator;
+
+const footer_generator = "<p>Copyright 2022 Nathan S. Hoskins.  Done for academic credit with Brigham Young University - Idaho, Online <br /> All rights reserved.  Images used are sourced from google and used in accordance to US Free Use Doctrine, Academic Calause.</P>"
+document.getElementById('footer').innerHTML = footer_generator;
