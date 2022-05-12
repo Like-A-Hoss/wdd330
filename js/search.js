2cca7d09de3b2323c4e2579737e1b02d8ba6db74
Like-A-Hoss <hapleswithdice@gmail.com>
2022-05-12 18:19:57 -0500

    Updated search html and js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,10 @@
+const input = form.elements.searchInput;
+input.addEventListener('change', () => alert('changed'), false);
+input.value = 'Search Here';
 const form = document.forms['search'];
 form.addEventListener ('submit', search, false);
 
-function search() {
-    alert(' Form Submitted');
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
 }
