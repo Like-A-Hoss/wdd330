7100e2445ff21bc457e022947bd96dea7042e824
Like-A-Hoss <hapleswithdice@gmail.com>
2022-05-28 15:20:33 -0500

    updated print task

--- js/todo/printtasks.js
+++ js/todo/printtasks.js
@@ -3,8 +3,8 @@
     for(task in toDoList ){
         let item = task[content];
         let id = task[id];
-        output += "<button class = 'completeButton'> </button>"
+        output += "<button class = 'completeButton' onclick='completeTask()' value='"+id+ "> </button>";
         output += "<label class = '' id='item_"+ id+"'>" +item+"</label>";
-        output += "<button class = 'deleteButton' onClick(removeTask())> X</button>"
+        output += "<button class = 'deleteButton' onClick='removeTask()' value='"+id+"'> X</button>";
     }
 
