8951b56abe5affac8293e541c15677d5e8ae875c
Like-A-Hoss <hapleswithdice@gmail.com>
2022-05-28 14:54:42 -0500

    added print tasks

--- js/todo/printtasks.js
+++ js/todo/printtasks.js
@@ -1,9 +1,10 @@
     let toDoList = localStorage.getItem("toDoList");
     let output = " "
-    for(id in toDoList ){
-        let item = id[content]
+    for(task in toDoList ){
+        let item = task[content];
+        let id = task[id];
         output += "<button class = 'completeButton'> </button>"
-        output += item;
+        output += "<label class = '' id='item_"+ id+"'>" +item+"</label>";
         output += "<button class = 'deleteButton' onClick(removeTask())> X</button>"
     }
 
